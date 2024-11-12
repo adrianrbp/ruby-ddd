35b442f65626718e0ca143e17567959adb21dab4
adrianrbp <eadrian.peralta@gmail.com>
2024-11-12 17:19:37 +0100

    feat: #2 insert and return transaction money

--- snack-machine/lib/logic/snack_machine.rb
+++ snack-machine/lib/logic/snack_machine.rb
@@ -1,7 +1,13 @@
 module Logic
   class SnackMachine
-    attr_reader :one_cent_count, :ten_cent_count, :quarter_count,
-                :one_dollar_count, :five_dollar_count, :twenty_dollar_count
+    # Money inside
+    attr_reader :one_cent_count, :ten_cent_count, 
+                :quarter_count, :one_dollar_count, 
+                :five_dollar_count, :twenty_dollar_count
+    # Money inserted by user
+    attr_reader :one_cent_count_in_transaction, :ten_cent_count_in_transaction,
+                :quarter_count_in_transaction, :one_dollar_count_in_transaction,
+                :five_dollar_count_in_transaction, :twenty_dollar_count_in_transaction
 
     def initialize
       @one_cent_count = 0
@@ -10,6 +16,13 @@ module Logic
       @one_dollar_count = 0
       @five_dollar_count = 0
       @twenty_dollar_count = 0
+
+      @one_cent_count_in_transaction = 0
+      @ten_cent_count_in_transaction = 0
+      @quarter_count_in_transaction = 0
+      @one_dollar_count_in_transaction = 0
+      @five_dollar_count_in_transaction = 0
+      @twenty_dollar_count_in_transaction = 0
     end
 
     def insert_money(
@@ -20,12 +33,21 @@ module Logic
         five_dollar_count, 
         twenty_dollar_count
       )
-      @one_cent_count += one_cent_count
-      @ten_cent_count += ten_cent_count
-      @quarter_count += quarter_count
-      @one_dollar_count += one_dollar_count
-      @five_dollar_count += five_dollar_count
-      @twenty_dollar_count += twenty_dollar_count
+      @one_cent_count_in_transaction += one_cent_count
+      @ten_cent_count_in_transaction += ten_cent_count
+      @quarter_count_in_transaction += quarter_count
+      @one_dollar_count_in_transaction += one_dollar_count
+      @five_dollar_count_in_transaction += five_dollar_count
+      @twenty_dollar_count_in_transaction += twenty_dollar_count
+    end
+
+    def return_money
+      @one_cent_count_in_transaction = 0
+      @ten_cent_count_in_transaction = 0
+      @quarter_count_in_transaction = 0
+      @one_dollar_count_in_transaction = 0
+      @five_dollar_count_in_transaction = 0
+      @twenty_dollar_count_in_transaction = 0
     end
   end
 end
--- snack-machine/test/test_snack_machine.rb
+++ snack-machine/test/test_snack_machine.rb
@@ -13,28 +13,47 @@ class SnackMachineTest < Minitest::Test
     assert_equal 0, @snack_machine.one_dollar_count
     assert_equal 0, @snack_machine.five_dollar_count
     assert_equal 0, @snack_machine.twenty_dollar_count
+
+    assert_equal 0, @snack_machine.one_cent_count_in_transaction
+    assert_equal 0, @snack_machine.ten_cent_count_in_transaction
+    assert_equal 0, @snack_machine.quarter_count_in_transaction
+    assert_equal 0, @snack_machine.one_dollar_count_in_transaction
+    assert_equal 0, @snack_machine.five_dollar_count_in_transaction
+    assert_equal 0, @snack_machine.twenty_dollar_count_in_transaction
   end
 
-  def test_insert_money_increments_counts
+  def test_insert_money_increments_transaction_counts
     @snack_machine.insert_money(1, 2, 3, 1, 0, 1)
 
-    assert_equal 1, @snack_machine.one_cent_count
-    assert_equal 2, @snack_machine.ten_cent_count
-    assert_equal 3, @snack_machine.quarter_count
-    assert_equal 1, @snack_machine.one_dollar_count
-    assert_equal 0, @snack_machine.five_dollar_count
-    assert_equal 1, @snack_machine.twenty_dollar_count
+    assert_equal 1, @snack_machine.one_cent_count_in_transaction
+    assert_equal 2, @snack_machine.ten_cent_count_in_transaction
+    assert_equal 3, @snack_machine.quarter_count_in_transaction
+    assert_equal 1, @snack_machine.one_dollar_count_in_transaction
+    assert_equal 0, @snack_machine.five_dollar_count_in_transaction
+    assert_equal 1, @snack_machine.twenty_dollar_count_in_transaction
   end
 
   def test_insert_money_multiple_times
     @snack_machine.insert_money(1, 1, 1, 1, 1, 1)
     @snack_machine.insert_money(2, 2, 2, 2, 2, 2)
 
-    assert_equal 3, @snack_machine.one_cent_count
-    assert_equal 3, @snack_machine.ten_cent_count
-    assert_equal 3, @snack_machine.quarter_count
-    assert_equal 3, @snack_machine.one_dollar_count
-    assert_equal 3, @snack_machine.five_dollar_count
-    assert_equal 3, @snack_machine.twenty_dollar_count
+    assert_equal 3, @snack_machine.one_cent_count_in_transaction
+    assert_equal 3, @snack_machine.ten_cent_count_in_transaction
+    assert_equal 3, @snack_machine.quarter_count_in_transaction
+    assert_equal 3, @snack_machine.one_dollar_count_in_transaction
+    assert_equal 3, @snack_machine.five_dollar_count_in_transaction
+    assert_equal 3, @snack_machine.twenty_dollar_count_in_transaction
+  end
+
+  def test_return_money_resets_transaction_counts
+    @snack_machine.insert_money(1, 2, 3, 4, 5, 6)
+    @snack_machine.return_money
+
+    assert_equal 0, @snack_machine.one_cent_count_in_transaction
+    assert_equal 0, @snack_machine.ten_cent_count_in_transaction
+    assert_equal 0, @snack_machine.quarter_count_in_transaction
+    assert_equal 0, @snack_machine.one_dollar_count_in_transaction
+    assert_equal 0, @snack_machine.five_dollar_count_in_transaction
+    assert_equal 0, @snack_machine.twenty_dollar_count_in_transaction
   end
 end
